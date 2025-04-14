0aa06a287567a52f0f8076a87ee4a05774d908f7
Angelina Faggiano <adeluca43@gmail.com>
2025-04-14 11:31:12 -0500

    Phase 1 complete

---
+++ Member.cs
@@ -0,0 +1,8 @@
+using System.Dynamic;
+
+public class Member
+{
+    public string Name { get; set; }
+    public int SkillLevel { get; set; }
+    public decimal CourageFactor { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Plan Your Heist!");
+
+Member member = new Member();
+
+Console.WriteLine("Enter a team member's name:");
+member.Name = Console.ReadLine();
+
+Console.WriteLine("Please enter a Skill level:");
+member.SkillLevel = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter a Courage Factor:");
+decimal courage = decimal.Parse(Console.ReadLine());
+while (courage > 2 || courage < 0)
+{
+    Console.WriteLine(" Please enter a number between 0.0 - 2.0");
+    courage = decimal.Parse(Console.ReadLine());
+}
+member.CourageFactor = courage;
+
+
+Console.WriteLine($"The team member's Name is {member.Name}. Their skill level is {member.SkillLevel}. Their courage factor is {member.CourageFactor}.");
